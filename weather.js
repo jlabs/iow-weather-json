b7a798eb81107175e2df3cb2fc5614edcb62f80c
Jamie <jamie@matrixcreate.com>
2022-07-25 23:43:12 +0100

    Moved weather stuff to own JS and added sample Node API routing

---
+++ weather.js
@@ -0,0 +1,16 @@
+const { parse } = require('rss-to-json');
+
+// Promise
+
+parse('http://www.isleofwightweather.com/rss.xml').then(rss => {
+    const data = JSON.stringify(rss, (k,v) => v === undefined ? null : v, 3);
+    return (data);
+})
+.then(data => {
+    return JSON.parse(data);
+})
+.then(conditions => {
+    const current = conditions.items[0];
+    const details = current.description.split(' | ');
+    console.log(details);
+});
